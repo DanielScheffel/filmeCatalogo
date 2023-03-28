f91adb83ea6dec1759bc0d8ca0b5901d33b8ce4a
ScheffelDaniel <scheffeldaniel499@gmail.com>
2023-03-28 11:04:48 -0300

    Estilizando e definindo cardDetalhes

--- js/classes.js
+++ js/classes.js
@@ -31,29 +31,29 @@ class Filme{
 
     getCard = async () => {
 
-        let card = document.createElement("div");
+        const card = document.createElement("div");
         card.setAttribute("class", "card");
 
-        let imgCartaz = document.createElement("img");
+        const imgCartaz = document.createElement("img");
         imgCartaz.setAttribute("class", "card-img-topz");
         imgCartaz.setAttribute("src", this.cartaz);
 
-        let cardBody = document.createElement("div");
+        const cardBody = document.createElement("div");
         cardBody.setAttribute("class", "card-body");
 
-        let hCardTitle = document.createElement("h5");
+        const hCardTitle = document.createElement("h5");
         hCardTitle.setAttribute("class", "card-title");
 
-        let divDetalhes = document.createElement("div");
+        const divDetalhes = document.createElement("div");
         divDetalhes.setAttribute("style", "display: flex; justify-content: space-around; text-align: center; font-weight: 700;");
 
-        let divGenero = document.createElement("div");
+        const divGenero = document.createElement("div");
         divGenero.setAttribute("style", "flex-grow:1;");
 
-        let divAnoProducao = document.createElement("div");
+        const divAnoProducao = document.createElement("div");
         divAnoProducao.setAttribute("style", "flex-grow:1;");
 
-        let divClassificacao = document.createElement("div");
+        const divClassificacao = document.createElement("div");
         divClassificacao.setAttribute("style", "flex-grow:1;");
 
         hCardTitle.appendChild(document.createTextNode(this.titulo));
@@ -94,59 +94,60 @@ class Filme{
 
     getDetalhesFilme =  () => {
 
-        let cardDetalhes = document.createElement('div');
+        const cardDetalhes = document.createElement('div');
         cardDetalhes.setAttribute("class", "card-detalhes");
 
-        let imgDetalhes = document.createElement("img");
+        const imgDetalhes = document.createElement("img");
         imgDetalhes.setAttribute("class", "img-detalhes");
         imgDetalhes.setAttribute("src", this.cartaz);
 
-        let cardBodyDetalhes = document.createElement("div");
+        const cardBodyDetalhes = document.createElement("div");
         cardBodyDetalhes.setAttribute("class", "body-detalhes");
 
-        let hTitleDetalhes = document.createElement("h5");
-        hTitleDetalhes.setAttribute("class", "card-title");
+        const hTitleDetalhes = document.createElement("h2");
+        hTitleDetalhes.setAttribute("class", "cardTitle");
         hTitleDetalhes.appendChild(document.createTextNode(this.titulo));
 
-        let divDetalhes = document.createElement("div");
+        const divDetalhes = document.createElement("div");
         divDetalhes.setAttribute("class", "div-detalhes");
 
-        let divAno = document.createElement("div");
+        const divAno = document.createElement("div");
         divAno.setAttribute("class", "div-ano");
+        divAno.appendChild(document.createTextNode("Ano: "));
 
-        let divGenero = document.createElement("div");
+        const divGenero = document.createElement("div");
         divGenero.setAttribute("class", "div-genero");
 
-        let divDuracao = document.createElement("div");
+        const divDuracao = document.createElement("div");
         divDuracao.setAttribute("class", "div-duracao");
 
-        let divDirecao = document.createElement("div");
+        const divDirecao = document.createElement("div");
         divDirecao.setAttribute("class", "div-direcao");
 
-        let divElenco = document.createElement("div");
+        const divElenco = document.createElement("div");
         divElenco.setAttribute("class", "div-elenco");
 
-        let divAvaliacao = document.createElement("div");
-        divAvaliacao.setAttribute("class", "div-avaliacao");
-
-        let divSinopse = document.createElement("div");
+        const divSinopse = document.createElement("div");
         divSinopse.setAttribute("class", "div-sinopse");
+
+        const divAvaliacao = document.createElement("div");
+        divAvaliacao.setAttribute("class", "div-avaliacao");
 
         divAno.appendChild(document.createTextNode(this.ano));
         divGenero.appendChild(document.createTextNode(this.genero));
         divDuracao.appendChild(document.createTextNode(this.duracao));
         divDirecao.appendChild(document.createTextNode(this.direcao));
         divElenco.appendChild(document.createTextNode(this.elenco));
-        divAvaliacao.appendChild(document.createTextNode(this.avaliacao));
         divSinopse.appendChild(document.createTextNode(this.sinopse));
+        divAvaliacao.appendChild(document.createTextNode(this.avaliacao));
 
         divDetalhes.appendChild(divAno);
         divDetalhes.appendChild(divGenero);
         divDetalhes.appendChild(divDuracao);
         divDetalhes.appendChild(divDirecao);
         divDetalhes.appendChild(divElenco);
-        divDetalhes.appendChild(divAvaliacao);
         divDetalhes.appendChild(divSinopse);
+        divDetalhes.appendChild(divAvaliacao);
 
         cardDetalhes.appendChild(imgDetalhes);
         cardDetalhes.appendChild(cardBodyDetalhes);
